0d4633e4cfadf128dbe41633030cfb83d6dd1039
SoPlays <joao.ranger@gmail.com>
2021-11-06 21:38:59 +0000

    ClassRoomLoaderTest done
    Took 34 minutes

--- src/main/java/pt/iscte/asd/projectN3/group11/loaders/ClassRoomLoader.java
+++ src/main/java/pt/iscte/asd/projectN3/group11/loaders/ClassRoomLoader.java
@@ -8,57 +8,52 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.LinkedList;
 
 public class ClassRoomLoader {
 
-    /**
-     * Loads a Classroom csv file.
-     * @param path path to the classroom csv.
-     * @return List of classrooms
-     */
-    public static final LinkedList<Classroom> load(final String path)
-    {
-        final LinkedList<Classroom> classrooms = new LinkedList<>();
-        try (
-                final Reader reader = Files.newBufferedReader(Paths.get(path));
-                final CSVReader csvReader = new CSVReader(reader)
-        ) {
+	/**
+	 * Loads a Classroom csv file.
+	 *
+	 * @param path path to the classroom csv.
+	 * @return List of classrooms
+	 */
+	public static final LinkedList<Classroom> load(final String path) {
+		final LinkedList<Classroom> classrooms = new LinkedList<>();
+		try (
+				final Reader reader = Files.newBufferedReader(Paths.get(path));
+				final CSVReader csvReader = new CSVReader(reader)
+		) {
 
-            final String[] headers = csvReader.readNext();
+			final String[] headers = csvReader.readNext();
 
-            String[] nextRecord;
-            if (Arrays.equals(headers, Classroom.CHARACTERISTICS_LIST)) {
-                System.out.println("ClassRoomLoad.main::Arrays are equal, loading...");
+			String[] nextRecord;
+			System.out.println("ClassRoomLoad.main::Arrays are equal, loading...");
 
-                while ((nextRecord = csvReader.readNext()) != null) {
-                    final String building = nextRecord[0];
-                    final String classroomName = nextRecord[1];
-                    final int normal_capacity = Integer.parseInt(nextRecord[2]);
-                    final int exam_capacity = Integer.parseInt(nextRecord[3]);
-                    final int number_characteristics = Integer.parseInt(nextRecord[4]);
-                    final LinkedList<Boolean> characteristics = new LinkedList<>();
+			while ((nextRecord = csvReader.readNext()) != null) {
+				final String building = nextRecord[0];
+				final String classroomName = nextRecord[1];
+				final int normal_capacity = Integer.parseInt(nextRecord[2]);
+				final int exam_capacity = Integer.parseInt(nextRecord[3]);
+				final int number_characteristics = Integer.parseInt(nextRecord[4]);
+				final LinkedList<Boolean> characteristics = new LinkedList<>();
 
-                    for (int index = 5; index < nextRecord.length; index++) {
-                        if (nextRecord[index].equals("X")) {
-                            characteristics.add(true);
-                        } else {
-                            characteristics.add(false);
-                        }
-                    }
-                    Classroom classN = new Classroom(characteristics, building, classroomName, normal_capacity, exam_capacity, number_characteristics);
-                    classrooms.add(classN);
-                }
-                System.out.println("ClassRoomLoad.main::classRooms = " + classrooms);
-            } else {
-                System.out.println("ClassRoomLoad.main::Arrays are NOT equal, exiting...");
+				for (int index = 5; index < nextRecord.length; index++) {
+					if (nextRecord[index].equals("X") || nextRecord[index].equals("x")) {
+						characteristics.add(true);
+					} else {
+						characteristics.add(false);
+					}
+				}
+				Classroom classN = new Classroom(characteristics, building, classroomName, normal_capacity, exam_capacity, number_characteristics);
+				classrooms.add(classN);
+			}
+			System.out.println("ClassRoomLoad.main::classRooms = " + classrooms);
 
-            }
-        } catch (IOException | CsvValidationException e) {
-            e.printStackTrace();
-        }
-        return classrooms;
-    }
+		} catch (IOException | CsvValidationException e) {
+			e.printStackTrace();
+		}
+		return classrooms;
+	}
 
 }
