1f743875898a3541f6277b59f70abfe4ebd5f4db
SoPlays <joao.ranger@gmail.com>
2021-11-27 23:36:43 +0000

    Added Final keywords

--- src/main/java/pt/iscte/asd/projectN3/group11/Managers/ClassRoomManager.java
+++ src/main/java/pt/iscte/asd/projectN3/group11/Managers/ClassRoomManager.java
@@ -13,7 +13,7 @@ public class ClassRoomManager {
 	}
 
 	public final List<Classroom> getWithCapacity(int  capacity){
-		LinkedList<Classroom> result = new LinkedList<>();
+		final LinkedList<Classroom> result = new LinkedList<>();
 		for (Classroom x :this.classrooms){
 			if (x.getNormalCapacity() >= capacity){
 				result.add(x);
@@ -22,7 +22,7 @@ public class ClassRoomManager {
 		return result;
 	}
 	public final List<Classroom> getWithExamCapacity(int  capacity){
-		LinkedList<Classroom> result = new LinkedList<>();
+		final LinkedList<Classroom> result = new LinkedList<>();
 		for (Classroom x :this.classrooms){
 			if (x.getExamCapacity() >= capacity){
 				result.add(x);
@@ -32,7 +32,7 @@ public class ClassRoomManager {
 	}
 
 	public final List<Classroom> getWithCharacteristics(List<String> characteristics){
-		LinkedList<Classroom> result = new LinkedList<>();
+		final LinkedList<Classroom> result = new LinkedList<>();
 		for (Classroom x :this.classrooms){
 			if (x.hasALLCharacteristics(characteristics )){
 				result.add(x);
@@ -42,7 +42,7 @@ public class ClassRoomManager {
 	}
 
 	public final List<Classroom> getWithCharacteristic(String characteristic){
-		LinkedList<Classroom> result = new LinkedList<>();
+		final LinkedList<Classroom> result = new LinkedList<>();
 		for (Classroom x :this.classrooms){
 				if (x.hasCharacteristic(characteristic )){
 					result.add(x);
@@ -52,7 +52,7 @@ public class ClassRoomManager {
 	}
 
 	public final List<Classroom> getInBuilding(String building){
-		LinkedList<Classroom> result = new LinkedList<>();
+		final LinkedList<Classroom> result = new LinkedList<>();
 		for (Classroom x :this.classrooms){
 			if (x.isInBuilding(building)){
 				result.add(x);
@@ -61,7 +61,7 @@ public class ClassRoomManager {
 		return result;
 	}
 	public final List<Classroom> getInAnyBuilding(List<String> buildings){
-		LinkedList<Classroom> result = new LinkedList<>();
+		final LinkedList<Classroom> result = new LinkedList<>();
 		for (Classroom x :this.classrooms){
 			if (x.isInANYBuilding(buildings)){
 				result.add(x);
