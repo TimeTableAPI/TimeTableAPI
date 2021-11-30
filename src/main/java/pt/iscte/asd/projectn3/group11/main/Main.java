d024ec7f12840a9f96c75768d1110a05ec97a065
SoPlays <joao.ranger@gmail.com>
2021-11-30 22:30:46 +0000

    Version Increase Moved Spring Main to Main.java

--- src/main/java/pt/iscte/asd/projectn3/group11/main/Main.java
+++ src/main/java/pt/iscte/asd/projectn3/group11/main/Main.java
@@ -1,8 +1,11 @@
 package pt.iscte.asd.projectn3.group11.main;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import pt.iscte.asd.projectn3.group11.loaders.ClassLoader;
 import pt.iscte.asd.projectn3.group11.loaders.ClassRoomLoader;
 
+@SpringBootApplication
 public class Main {
     private static final String SAMPLE_CSV_FILE_CLASS_PATH = "./src/main/resources/ADS - Exemplo de horario do 1o Semestre.csv";
     private static final String SAMPLE_CSV_FILE_CLASSROOM_PATH = "./src/main/resources/ADS - Caracterizacao das salas.csv";
@@ -10,5 +13,8 @@ public class Main {
     public static void main(String[] args) {
         ClassLoader.load(SAMPLE_CSV_FILE_CLASS_PATH);
         ClassRoomLoader.load(SAMPLE_CSV_FILE_CLASSROOM_PATH);
+
+        SpringApplication.run(Main.class, args);
+        System.out.println("Main App Started with SPRING");
     }
 }
