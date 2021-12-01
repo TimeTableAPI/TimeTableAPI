cee9ecf70ee40592dedcb5033296adc2a55d0da1
SoPlays <joao.ranger@gmail.com>
2021-12-01 20:53:46 +0000

    changed dateMap from HashMap<ClassCourseDate, List<ClassCourse>> to HashMap<ClassCourseDate, EnumMap<ClassCourseTime, List<ClassCourse>>>; added classRoomdateMap to store classRooms and the date and time at which they are occupied

--- src/main/java/pt/iscte/asd/projectn3/group11/services/algorithms/BasicAlgorithmService.java
+++ src/main/java/pt/iscte/asd/projectn3/group11/services/algorithms/BasicAlgorithmService.java
@@ -5,32 +5,54 @@ import pt.iscte.asd.projectn3.group11.models.Classroom;
 import pt.iscte.asd.projectn3.group11.models.util.ClassCourseDate;
 import pt.iscte.asd.projectn3.group11.models.util.ClassCourseTime;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class BasicAlgorithmService implements IAlgorithmService {
 
     @Override
     public void execute(List<ClassCourse> classCourses, List<Classroom> classrooms) {
         System.out.println("BASIC_ALGORITHM::EXECUTE");
-        HashMap<ClassCourseDate, List<ClassCourse>> dateMap = new HashMap<>();
+
+        HashMap<ClassCourseDate, EnumMap<ClassCourseTime, List<ClassCourse>>> classCoursedateMap = initializeClassCourseDateMap(classCourses);
+        HashMap<ClassCourseDate, EnumMap<ClassCourseTime, List<Classroom>>> classRoomdateMap = initializeClassRoomDateMap(classCoursedateMap);
+
+        for(Map.Entry<ClassCourseDate, EnumMap<ClassCourseTime, List<ClassCourse>>> classCourseOfDay: classCoursedateMap.entrySet())
+        {
+            System.out.println(classCourseOfDay.getKey() + " -> " + classCourseOfDay.getValue().size());
+            for(Map.Entry<ClassCourseTime, List<ClassCourse>> classCourseOfDayOfTime : classCourseOfDay.getValue().entrySet())
+            {
+                System.out.println(classCourseOfDayOfTime.getKey() + " -> " + classCourseOfDayOfTime.getValue().size());
+                for(ClassCourse classCourse : classCourseOfDayOfTime.getValue() ){
+                    System.out.println(classCourse);
+
+                }
+            }
+        }
+    }
+
+    private HashMap<ClassCourseDate, EnumMap<ClassCourseTime, List<ClassCourse>>> initializeClassCourseDateMap(List<ClassCourse> classCourses) {
+        HashMap<ClassCourseDate, EnumMap<ClassCourseTime, List<ClassCourse>>> classCoursedateMap = new HashMap<>();
         for(ClassCourse classCourse: classCourses)
         {
-            dateMap.computeIfAbsent(classCourse.getDate(), k -> new ArrayList<>());
-            dateMap.get(classCourse.getDate()).add(classCourse);
+            classCoursedateMap.computeIfAbsent(classCourse.getDate(), k -> new EnumMap<ClassCourseTime, List<ClassCourse>>(ClassCourseTime.class));
+            classCoursedateMap.get(classCourse.getDate()).computeIfAbsent(classCourse.getBeginningHour(), k -> new ArrayList<>());
+            classCoursedateMap.get(classCourse.getDate()).get(classCourse.getBeginningHour()).add(classCourse);
+
         }
+        return classCoursedateMap;
+    }
 
-        for(Map.Entry<ClassCourseDate, List<ClassCourse>> set: dateMap.entrySet())
-        {
-            for(ClassCourseTime classCourseTime : ClassCourseTime.values())
-            {
+    private HashMap<ClassCourseDate, EnumMap<ClassCourseTime, List<Classroom>>> initializeClassRoomDateMap(HashMap<ClassCourseDate, EnumMap<ClassCourseTime, List<ClassCourse>>> classCoursedateMap) {
+        HashMap<ClassCourseDate, EnumMap<ClassCourseTime, List<Classroom>>>classRoomDateMap = new HashMap<>();
 
+        for (Map.Entry<ClassCourseDate, EnumMap<ClassCourseTime, List<ClassCourse>>> date : classCoursedateMap.entrySet()) {
+            for (Map.Entry<ClassCourseTime, List<ClassCourse>> hour : date.getValue().entrySet()) {
+                System.out.println();
+                classRoomDateMap.computeIfAbsent(date.getKey(), k ->new EnumMap<ClassCourseTime, List<Classroom>>(ClassCourseTime.class));
+                classRoomDateMap.get(date.getKey()).computeIfAbsent(hour.getKey(), k-> new ArrayList<Classroom>());
             }
-            System.out.println(set.getKey() + " -> " + set.getValue().size());
+
         }
-
-
+        return classRoomDateMap;
     }
 }
