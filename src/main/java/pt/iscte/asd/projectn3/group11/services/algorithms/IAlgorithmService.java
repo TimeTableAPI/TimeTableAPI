265046f99c8d7c9049215680e2928f4c7c9f1c1f
AfonsoCostaVale <afonsocostavale@protonmail.com>
2021-12-15 00:23:38 +0000

    IAlgorithmService documentation

--- src/main/java/pt/iscte/asd/projectn3/group11/services/algorithms/IAlgorithmService.java
+++ src/main/java/pt/iscte/asd/projectn3/group11/services/algorithms/IAlgorithmService.java
@@ -6,5 +6,10 @@ import pt.iscte.asd.projectn3.group11.models.ClassCourse;
 import java.util.List;
 
 public interface IAlgorithmService {
+    /**
+     * Executes the algorithm population the classes.
+     * @param classes
+     * @param classrooms
+     */
     public void execute(List<ClassCourse> classes, List<Classroom> classrooms);
 }
