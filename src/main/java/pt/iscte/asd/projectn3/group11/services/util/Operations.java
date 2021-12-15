6df9f42157dc220fd01db1da460a894fe36ebcd5
TariqYoussef <tarek332000@proton.me>
2021-12-15 18:23:20 +0000

    major refactoring

--- src/main/java/pt/iscte/asd/projectn3/group11/services/util/Operations.java
+++ src/main/java/pt/iscte/asd/projectn3/group11/services/util/Operations.java
@@ -1,5 +1,7 @@
-package pt.iscte.asd.projectn3.group11.models.util;
+package pt.iscte.asd.projectn3.group11.services.util;
 
+import pt.iscte.asd.projectn3.group11.services.util.LogicOperation;
+@Deprecated
 public class Operations {
     public static final LogicOperation AND = ((a, b) -> a && b);
     public static final LogicOperation OR = ((a, b) -> a || b);
