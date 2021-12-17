e59da8e4ca4d08626b386360ec734b64ea27b062
AfonsoCostaVale <afonsocostavale@protonmail.com>
2021-12-17 12:37:43 +0000

    swrl api working

--- src/main/java/pt/iscte/asd/projectn3/group11/services/SwrlService.java
+++ src/main/java/pt/iscte/asd/projectn3/group11/services/SwrlService.java
@@ -14,54 +14,52 @@ import java.io.File;
 import java.util.LinkedList;
 import java.util.List;
 
-public class SwrlService
-{
+public class SwrlService {
 
-	public static final String OWLFILE = "src/main/resources/ADS.owl";
+    public static final String OWL_FILE = "src/main/resources/ADS.owl";
 
-	public static List<String> querry(int numberOfObjectivesQuerry)
-  {
-	File owlFile = new File(OWLFILE);
-    try {
-      // Loading an OWL ontology using the OWLAPI
-      OWLOntologyManager ontologyManager = OWLManager.createOWLOntologyManager();
-      OWLOntology ontology =  ontologyManager.loadOntologyFromOntologyDocument(owlFile);
+    public static List<String> querry(int numberOfObjectivesQuerry) {
+        File owlFile = new File(OWL_FILE);
+        try {
+            // Loading an OWL ontology using the OWLAPI
+            OWLOntologyManager ontologyManager = OWLManager.createOWLOntologyManager();
+            OWLOntology ontology = ontologyManager.loadOntologyFromOntologyDocument(owlFile);
 
-      // Create SQWRL query engine using the SWRLAPI
-      SQWRLQueryEngine queryEngine = SWRLAPIFactory.createSQWRLQueryEngine(ontology);
+            // Create SQWRL query engine using the SWRLAPI
+            SQWRLQueryEngine queryEngine = SWRLAPIFactory.createSQWRLQueryEngine(ontology);
 
-      // Create and execute a SQWRL query using the SWRLAPI
-      // Os algoritmos que funcionam bem para problemas com 2,3,4,etc. objetivps são diferentes
-      String numberOfObjectives =  Integer.toString(numberOfObjectivesQuerry);
-      String query = "Algorithm(?alg) ^ "   
-    	+ "minObjectivesAlgorithmIsAbleToDealWith(?alg,?min) ^ swrlb:lessThanOrEqual(?min,"+numberOfObjectives+")"
-    	+ "maxObjectivesAlgorithmIsAbleToDealWith(?alg,?max) ^ swrlb:greaterThanOrEqual(?max,"+numberOfObjectives+")"
-    	+ " -> sqwrl:select(?alg) ^ sqwrl:orderBy(?alg)";  
-      SQWRLResult result = queryEngine.runSQWRLQuery("q1", query);
+            // Create and execute a SQWRL query using the SWRLAPI
+            // Os algoritmos que funcionam bem para problemas com 2,3,4,etc. objetivps são diferentes
+            String numberOfObjectives = Integer.toString(numberOfObjectivesQuerry);
+            String query = "Algorithm(?alg) ^ "
+                    + "minObjectivesAlgorithmIsAbleToDealWith(?alg,?min) ^ swrlb:lessThanOrEqual(?min," + numberOfObjectives + ")"
+                    + "maxObjectivesAlgorithmIsAbleToDealWith(?alg,?max) ^ swrlb:greaterThanOrEqual(?max," + numberOfObjectives + ")"
+                    + " -> sqwrl:select(?alg) ^ sqwrl:orderBy(?alg)";
+            SQWRLResult result = queryEngine.runSQWRLQuery("q1", query);
 
-      // Process the SQWRL result
-	  System.out.println("Query: \n" + query + "\n");
-	  System.out.println("Result: ");
-      List<String> results = new LinkedList<>();
-	  while (result.next()) {
-    	  System.out.println(result.getNamedIndividual("alg").getShortName());
-		  results.add(result.getNamedIndividual("alg").getShortName());
-      }
+            // Process the SQWRL result
+            System.out.println("Query: \n" + query + "\n");
+            System.out.println("Result: ");
+            List<String> results = new LinkedList<>();
+            while (result.next()) {
+                System.out.println(result.getNamedIndividual("alg").getShortName());
+                results.add(result.getNamedIndividual("alg").getShortName());
+            }
 
-	  return results;
-      
-    } catch (OWLOntologyCreationException e) {
-      System.err.println("Error creating OWL ontology: " + e.getMessage());
-	  return null;
-    } catch (SWRLParseException e) {
-      System.err.println("Error parsing SWRL rule or SQWRL query: " + e.getMessage());
-	  return null;
-    } catch (SQWRLException e) {
-      System.err.println("Error running SWRL rule or SQWRL query: " + e.getMessage());
-	  return null;
-    } catch (RuntimeException e) {
-      System.err.println("Error starting application: " + e.getMessage());
-	  return null;
+            return results;
+
+        } catch (OWLOntologyCreationException e) {
+            System.err.println("Error creating OWL ontology: " + e.getMessage());
+            return null;
+        } catch (SWRLParseException e) {
+            System.err.println("Error parsing SWRL rule or SQWRL query: " + e.getMessage());
+            return null;
+        } catch (SQWRLException e) {
+            System.err.println("Error running SWRL rule or SQWRL query: " + e.getMessage());
+            return null;
+        } catch (RuntimeException e) {
+            System.err.println("Error starting application: " + e.getMessage());
+            return null;
+        }
     }
-  }
 }
