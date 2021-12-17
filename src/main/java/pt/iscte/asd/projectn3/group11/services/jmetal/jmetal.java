0f2d73f69e7692092dc5060eceebe7813d83cdd1
SoPlays <joao.ranger@gmail.com>
2021-12-17 03:47:53 +0000

    jmetal progress

--- src/main/java/pt/iscte/asd/projectn3/group11/services/jmetal/jmetal.java
+++ src/main/java/pt/iscte/asd/projectn3/group11/services/jmetal/jmetal.java
@@ -1,19 +1,70 @@
 package pt.iscte.asd.projectn3.group11.services.jmetal;
 
 import org.uma.jmetal.algorithm.Algorithm;
-import org.uma.jmetal.algorithm.multiobjective.nsgaii.NSGAII;
 import org.uma.jmetal.algorithm.multiobjective.nsgaii.NSGAIIBuilder;
+import org.uma.jmetal.operator.crossover.impl.PMXCrossover;
+import org.uma.jmetal.operator.mutation.impl.PermutationSwapMutation;
+import org.uma.jmetal.problem.Problem;
+import org.uma.jmetal.solution.permutationsolution.PermutationSolution;
 import pt.iscte.asd.projectn3.group11.Context;
+import pt.iscte.asd.projectn3.group11.models.ClassCourse;
+import pt.iscte.asd.projectn3.group11.models.Classroom;
+import pt.iscte.asd.projectn3.group11.services.TimetableEvaluationService;
+import pt.iscte.asd.projectn3.group11.services.algorithms.BasicAlgorithmService;
+import pt.iscte.asd.projectn3.group11.services.loaders.ClassCourseLoaderService;
+import pt.iscte.asd.projectn3.group11.services.loaders.ClassroomLoaderService;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class jmetal {
 	public static void main(String[] args) {
-		NSGAII<jmetalSolution> nsgai = new NSGAIIBuilder<jmetalSolution>(
-				new jmetalProblem(),
-				new jmetalCrossoverOperator(),
-				new jmetalMutationOperator(),
-				10
-		).build();
-		nsgai.run();
+		LinkedList<Classroom> loadedClassRooms = ClassroomLoaderService.load("src/main/resources/ADS - Caracterizacao das salas.csv");
+		LinkedList<ClassCourse> loadedClassCourses = ClassCourseLoaderService.load("src/main/resources/ADS - Exemplo de horario do 1o Semestre.csv");
+
+		Context context = new Context(loadedClassCourses, loadedClassRooms, new BasicAlgorithmService());
+
+		Problem<TimetableSolution> problem = new jmetalProblem(context, TimetableEvaluationService.ALLMETRICSLIST);
+		Algorithm< List<PermutationSolution<Integer>>> algorithmNSGAII = nsgaiii(problem);
+		long start = System.currentTimeMillis();
+		algorithmNSGAII.run();
+		long end = System.currentTimeMillis();
+		long elapsedTime = end - start;
+		System.out.println("Elapsed Time: " + elapsedTime );
+		List<PermutationSolution<Integer>> solutions = algorithmNSGAII.getResult();
+		System.out.println("Solutions: " + solutions);
 
 	}
+
+	private static void getBestResult(){
+
+	}
+
+	private static Algorithm< List<PermutationSolution<Integer>>> nsgaiii(Problem<TimetableSolution> problem){
+		int maxEvaluations = 1;
+		int populationSize = 10;
+		int matingPoolSize = 20;
+		int offspringPopulationSize = 20;
+
+		return new NSGAIIBuilder<TimetableSolution>(
+				problem,
+				new PMXCrossover(0.5),
+				new PermutationSwapMutation(0.5),
+				populationSize
+		).
+				setMaxEvaluations(maxEvaluations).
+				setMatingPoolSize(matingPoolSize).
+				setOffspringPopulationSize(offspringPopulationSize).
+				build();
+
+				/*maxEvaluations,
+				populationSize,
+				matingPoolSize,
+				offspringPopulationSize,
+				new
+				new jmetalSolutionEvaluator(),
+*/
+	}
+
+
 }
--- src/main/java/pt/iscte/asd/projectn3/group11/services/jmetal/jmetalProblem.java
+++ src/main/java/pt/iscte/asd/projectn3/group11/services/jmetal/jmetalProblem.java
@@ -1,35 +1,103 @@
 package pt.iscte.asd.projectn3.group11.services.jmetal;
 
 import org.uma.jmetal.problem.Problem;
+import org.uma.jmetal.problem.permutationproblem.PermutationProblem;
+import org.uma.jmetal.problem.permutationproblem.impl.AbstractIntegerPermutationProblem;
+import org.uma.jmetal.solution.permutationsolution.PermutationSolution;
+import pt.iscte.asd.projectn3.group11.Context;
+import pt.iscte.asd.projectn3.group11.models.ClassCourse;
+import pt.iscte.asd.projectn3.group11.models.Classroom;
+import pt.iscte.asd.projectn3.group11.services.TimetableEvaluationService;
+import pt.iscte.asd.projectn3.group11.services.util.metriccalculators.MetricCalculator;
 
-public class jmetalProblem implements Problem<jmetalSolution> {
+import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.List;
+
+public class jmetalProblem implements Problem<TimetableSolution> {
+	static int counter = 0 ;
+
+	private final List<Float> objectiveDirections = new LinkedList<>();
+
+	private final List<Classroom> variableClassrooms;
+	private final List<ClassCourse> variableClassCourses;
+	private final List<MetricCalculator> variableMetrics;
+	private final int numberOfVariables;
+	private final int numberOfObjectives;
+	private final int numberOfConstraints;
+
+
+	protected jmetalProblem(Context context, List<MetricCalculator> metrics) {
+		variableClassCourses = context.getClassCourses();
+		variableClassrooms = context.getClassrooms();
+		variableMetrics = metrics;
+		variableMetrics.forEach(metricCalculator -> objectiveDirections.add(metricCalculator.getObjective()));
+		numberOfVariables = Math.max(variableClassCourses.size(), variableClassrooms.size());
+		numberOfObjectives = variableMetrics.size();
+		numberOfConstraints = 0;
+
+	}
+
+	@Override
+	public TimetableSolution evaluate(TimetableSolution solution) {
+		final List<Integer> variables = solution.variables();
+		final double[] objectives = solution.objectives();
+
+		List<ClassCourse> createdTimetable = new LinkedList<>();
+		for(int i = 0; i < variables.size() && i < variableClassCourses.size() ; i++){
+			final Integer classroomIndex = variables.get(i);
+			final ClassCourse classCourse = variableClassCourses.get(i);
+			if (classroomIndex > -1) {
+				//classroom assigned
+				final Classroom classroom = variableClassrooms.get(classroomIndex);
+				classCourse.setClassroom(classroom);
+				createdTimetable.add(classCourse);
+			}else {
+				//no classroom assigned
+				createdTimetable.add(classCourse);
+			}
+		}
+		final Hashtable<String, Float> metricsHashTable = TimetableEvaluationService.evaluateTimetable(createdTimetable, variableClassrooms, variableMetrics);
+		for (int i = 0; i < variableMetrics.size(); i++) {
+			MetricCalculator metricCalculator = variableMetrics.get(i);
+			int finalI = i;
+			metricsHashTable.forEach((s, aFloat) -> {
+				if (metricCalculator.getClass().getSimpleName().equals(s)) {
+					objectives[finalI] = aFloat;
+				}
+			});
+		}
+		counter++;
+		System.out.println(counter);
+
+		return solution;
+	}
+
+	@Override
+	public TimetableSolution createSolution() {
+		return new TimetableSolution(getNumberOfVariables(),variableClassrooms.size(), getNumberOfObjectives());
+	}
+
 	@Override
 	public int getNumberOfVariables() {
-		return 0;
+		return numberOfVariables;
 	}
 
 	@Override
 	public int getNumberOfObjectives() {
-		return 0;
+		return numberOfObjectives;
 	}
 
 	@Override
 	public int getNumberOfConstraints() {
-		return 0;
+		return numberOfConstraints;
 	}
 
 	@Override
 	public String getName() {
-		return null;
+		return "";
 	}
 
-	@Override
-	public jmetalSolution evaluate(jmetalSolution jmetalSolution) {
-		return null;
-	}
 
-	@Override
-	public jmetalSolution createSolution() {
-		return null;
-	}
+
 }
