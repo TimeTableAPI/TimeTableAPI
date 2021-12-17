aab3629c6b77e5925f7325a8c7f9c4a25d71d89e
TariqYoussef <tarek332000@proton.me>
2021-12-17 00:58:02 +0000

    Infinity changed to Integer.MAX_VALUE

--- src/main/java/pt/iscte/asd/projectn3/group11/services/algorithms/BasicAlgorithmService.java
+++ src/main/java/pt/iscte/asd/projectn3/group11/services/algorithms/BasicAlgorithmService.java
@@ -9,9 +9,9 @@ import pt.iscte.asd.projectn3.group11.services.ClassroomService;
 import java.util.*;
 
 public class BasicAlgorithmService implements IAlgorithmService {
-//TODO
+
     /**
-     *
+     * Executes the algorithm.
      * @param classCourses
      * @param classrooms
      */
@@ -29,38 +29,5 @@ public class BasicAlgorithmService implements IAlgorithmService {
             }
         }
     }
-//TODO
-    /**
-     *
-     * @param classCourses
-     * @return
-     */
-    private TreeMap<Date, EnumMap<TimeShift, HashSet<ClassCourse>>> initializeClassCourseDateMap(List<ClassCourse> classCourses) {
-        TreeMap<Date, EnumMap<TimeShift, HashSet<ClassCourse>>> classCoursedateMap = new TreeMap<>();
-        for (ClassCourse classCourse : classCourses) {
-            classCoursedateMap.computeIfAbsent(classCourse.getDate(), k -> new EnumMap<TimeShift, HashSet<ClassCourse>>(TimeShift.class));
-            classCoursedateMap.get(classCourse.getDate()).computeIfAbsent(classCourse.getBeginningHour(), k -> new HashSet<ClassCourse>());
-            classCoursedateMap.get(classCourse.getDate()).get(classCourse.getBeginningHour()).add(classCourse);
-        }
-        return classCoursedateMap;
-    }
-//TODO
-    /**
-     *
-     * @param classCoursedateMap
-     * @return
-     */
-    private TreeMap<Date, EnumMap<TimeShift, HashSet<Classroom>>> initializeClassRoomDateMap(TreeMap<Date, EnumMap<TimeShift, HashSet<ClassCourse>>> classCoursedateMap) {
-        TreeMap<Date, EnumMap<TimeShift, HashSet<Classroom>>> classRoomDateMap = new TreeMap<>();
 
-        for (Map.Entry<Date, EnumMap<TimeShift, HashSet<ClassCourse>>> date : classCoursedateMap.entrySet()) {
-            for (Map.Entry<TimeShift, HashSet<ClassCourse>> hour : date.getValue().entrySet()) {
-                System.out.println();
-                classRoomDateMap.computeIfAbsent(date.getKey(), k -> new EnumMap<TimeShift, HashSet<Classroom>>(TimeShift.class));
-                classRoomDateMap.get(date.getKey()).computeIfAbsent(hour.getKey(), k -> new HashSet<Classroom>());
-            }
-
-        }
-        return classRoomDateMap;
-    }
 }
