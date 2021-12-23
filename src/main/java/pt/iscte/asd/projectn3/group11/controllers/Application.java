1453cd6b51dbf7e6683488c01b30290f0214e9d5
SoPlays <joao.ranger@gmail.com>
2021-12-23 23:57:14 +0000

    Log4Shell reproducible

--- src/main/java/pt/iscte/asd/projectn3/group11/controllers/Application.java
+++ src/main/java/pt/iscte/asd/projectn3/group11/controllers/Application.java
@@ -1,18 +1,26 @@
 package pt.iscte.asd.projectn3.group11.controllers;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import pt.iscte.asd.projectn3.group11.services.LoggerService;
 
 
+
 @ComponentScan({"pt.iscte.asd.projectn3.group11.controllers"})
 @SpringBootApplication
 public class Application {
+    public static final Logger LOGGER = LogManager.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         LoggerService.LOGGER.info("Main App Started with SPRING");
+        LoggerService.LOGGER.trace("trace");
+        LOGGER.error("${echo hello world}");
+        LOGGER.error("Value {}","${env:JAVA_HOME}" );
+        LOGGER.error("Value {}","${java:version}" );
     }
 
 }
