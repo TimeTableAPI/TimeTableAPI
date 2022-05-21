5b10e1a662963c99915cd219ba3ab569538df4a9
AfonsoCostaVale <afonsocostavale@protonmail.com>
2022-05-21 22:25:26 +0100

    system.out.println removed

--- src/test/java/pt/iscte/asd/projectn3/group11/services/TimetableEvaluationServiceTest.java
+++ src/test/java/pt/iscte/asd/projectn3/group11/services/TimetableEvaluationServiceTest.java
@@ -30,7 +30,7 @@ class TimetableEvaluationServiceTest {
 		classCoursesTestList = ClassCourseLoaderService.load(SAMPLE_CSV_FILE_CLASS_FILLED_PATH);
 
 		metricResultListTimetable = TimetableEvaluationService.evaluateTimetable(classCoursesTestList, classroomsTestList);
-		System.out.println(metricResultListTimetable);
+		LogService.getInstance().info(metricResultListTimetable);
 	}
 
 
